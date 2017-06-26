2ed22bdeb3de0cdf2dafb16ef63b7575d7673ea4
Ryan Ciecalone <ryan.ciecalone@issinc.com>
2017-06-26 17:23:31 -0600

    Updated angular titles

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Skeleton</h1>',
+    template: '<h1>Angular 4 Skeleton</h1>',
 })
 
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {TableBuilderAppModule} from '@aims/table-builder';
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [BrowserModule, TableBuilderAppModule],
+    declarations: [AppComponent, TableBuilderAppModule],
     bootstrap: [AppComponent]
 })
 
